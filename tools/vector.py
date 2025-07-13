49da10dc7faa9a8089582c60f5ce7d965d26ceb3
shyangwang <tai.shyang.wang@gmail.com>
2025-07-13 15:52:02 +0800

    Update vector.py

--- tools/vector.py
+++ tools/vector.py
@@ -53,3 +53,49 @@ plot_retriever = create_retrieval_chain(
 
 def get_movie_plot(input):
     return plot_retriever.invoke({"input": input})
+
+
+# neo4jvector = Neo4jVector.from_existing_index(
+#     embeddings,                              # (1)
+#     graph=graph,                             # (2)
+#     index_name="moviePlots",                 # (3)
+#     node_label="Movie",                      # (4)
+#     text_node_property="plot",               # (5)
+#     embedding_node_property="plotEmbedding", # (6)
+#     retrieval_query="""
+# RETURN
+#     node.plot AS text,
+#     score,
+#     {
+#         title: node.title,
+#         directors: [ (person)-[:DIRECTED]->(node) | person.name ],
+#         actors: [ (person)-[r:ACTED_IN]->(node) | [person.name, r.role] ],
+#         tmdbId: node.tmdbId,
+#         source: 'https://www.themoviedb.org/movie/'+ node.tmdbId
+#     } AS metadata
+# """
+# )
+
+# retriever = neo4jvector.as_retriever()
+
+# instructions = (
+#     "Use the given context to answer the question."
+#     "If you don't know the answer, say you don't know."
+#     "Context: {context}"
+# )
+
+# prompt = ChatPromptTemplate.from_messages(
+#     [
+#         ("system", instructions),
+#         ("human", "{input}"),
+#     ]
+# )
+
+# question_answer_chain = create_stuff_documents_chain(llm, prompt)
+# plot_retriever = create_retrieval_chain(
+#     retriever, 
+#     question_answer_chain
+# )
+
+# def get_movie_plot(input):
+#     return plot_retriever.invoke({"input": input})
